d36390b95ec23dbf289835f575a2a6e3b09bae88
Or Grinberg <or.grin@gmail.com>
2024-06-19 00:26:18 +0100

    Remove template residuals from header/footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,12 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <ul>
-        <li><a href="https://loco.rs?ref=starter" target="_blank" rel="noopener noreferrer">Our Documentation</a></li>
-        <li><a href="https://github.com/loco-rs/loco?ref=starter" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-        <li><a href="https://github.com/loco-rs/loco/issues?ref=starter" target="_blank" rel="noopener noreferrer">Found a bug?</a></li>
-        <li><a href="https://github.com/loco-rs/loco/discussions?ref=starter" target="_blank" rel="noopener noreferrer">Needs help?</a></li>
-      </ul>
+      <h1></h1>
     </footer>
   );
 };
