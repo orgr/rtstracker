32c97c3db0bf63350d6cc5431aeaef1688eb619b
Or Grinberg <or.grin@gmail.com>
2024-06-29 16:21:29 +0100

    Improve responsiveness

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header className='navbar fixed-top'>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <div className='container'>
         <Link to='/' className='navbar-brand'>RTS Tracker</Link>
       </div>
